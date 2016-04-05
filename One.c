aedb59cf7fcd5f94594b05c9182a02c0710f355d
guilhermedanjos <dosanjos_guilherme@hotmail.com>
2016-04-05 15:04:25 -0300

    Added files via upload

---
+++ One.c
@@ -0,0 +1,15 @@
+/*
+ * One.c
+ *
+ *  Created on: Apr 5, 2016
+ *      Author: guilherme
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("Hi Finally\n\nDude it`s runnnig\n\nHi hi hi hi hi");
+	return 0;
+}
